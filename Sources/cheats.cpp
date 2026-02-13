a77f0417bea51c964cb9e9af926bfd86c7179300
zirou1502 <kanahebizirou1502@gmail.com>
2026-02-13 13:38:46 +0900

    add フォントテスト用チート

--- Sources/cheats.cpp
+++ Sources/cheats.cpp
@@ -1,93 +1,38 @@
 #include "cheats.hpp"
 
-#include <freetype2/ft2build.h>
-
 #include "Memory/MemManager.hpp"
+#include "Render/Font.hpp"
+
 #include FT_FREETYPE_H
 
 namespace CTRPluginFramework {
 
-void test(MenuEntry* entry) {
-    int res = 0;
-    FT_Library library;
-    FT_Face face;
-    char path[256];
-    std::string tid;
-    u8* buf;
-
-    res = FT_Init_FreeType(&library);
-    if (res != 0) {
-        MessageBox("Ft_Init failed")();
-        return;
-    }
-
-    Process::GetTitleID(tid);
-    sprintf(path, "/luma/plugins/%s/fonts/tom-thumb.otf", tid.c_str());
-    res = File::Exists(path);
-    if (res == 0) {
-        MessageBox("File doesn't exist")();
-    }
-
-    File file(path, File::READ);
-    if (!file.IsOpen()) {
-        MessageBox("File open failed")();
-        FT_Done_FreeType(library);
-        return;
-    }
-
-    u32 size = file.GetSize();
-    if (size == 0) {
-        MessageBox("File size is 0")();
-        FT_Done_FreeType(library);
-        return;
-    }
-
-    buf = (u8*)calloc(sizeof(u8), size);
-    if (buf == nullptr) {
-        MessageBox("Could not alloc memory")();
-    }
-
-    file.Read(buf, size);
-    res = FT_New_Memory_Face(library, buf, size, 0, &face);
-    if (res != 0) {
-        MessageBox("Could not open font")();
-        free(buf);
-        FT_Done_FreeType(library);
-        return;
-    }
-
-    FT_Set_Pixel_Sizes(face, 0, 20);
-    res = FT_Load_Char(face, 'A', FT_LOAD_RENDER);
-    if (res != 0) {
-        MessageBox("Could not load char")();
-        free(buf);
-        FT_Done_Face(face);
-        FT_Done_FreeType(library);
-        return;
-    }
-
-    FT_GlyphSlot slot = face->glyph;
-    FT_Bitmap& bmp = slot->bitmap;
-
-    const Screen& scr = OSD::GetTopScreen();
+bool DrawOSD(const Screen& scr) {
+    if (!scr.IsTop) return false;
+    Font font("Leipzig2.ttf");
+    font.SetFontSize(50);
     int pos_x = 100;
     int pos_y = 120;
 
-    for (int i = 0; i < bmp.rows; i++) {
-        for (int j = 0; j < bmp.width; j++) {
-            unsigned char c = bmp.buffer[i * bmp.pitch + j];
-            if (c > 0) {
-                scr.DrawPixel(pos_x + j + slot->bitmap_left, pos_y + i + slot->bitmap_top, Color::White);
+    std::string str = "Hello World";
+
+    for (char ch : str) {
+        font.LoadChar(ch);
+        Font::GlyphInfo info = font.GetGlyphInfo();
+
+        for (int i = 0; i < info.rows; i++)
+            for (int j = 0; j < info.width; j++) {
+                u8 c = info.buf[i * info.pitch + j];
+                scr.DrawPixel(pos_x + j + info.left, pos_y - info.top + i, (c > 0 ? Color::White : Color::Black));
             }
-        }
+
+        pos_x += info.width;
     }
+    return true;
+}
 
-    OSD::SwapBuffers();
-    Sleep(Seconds(5));
-
-    free(buf);
-    FT_Done_Face(face);
-    FT_Done_FreeType(library);
+void test(MenuEntry* entry) {
+    OSD::Run(DrawOSD);
     return;
 }
 
